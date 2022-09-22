7e3f8d2c43cc0310c08f30052c86f39b98f46980
NaufalAndhikaF <113429898+NaufalAndhikaF@users.noreply.github.com>
2022-09-22 11:52:21 +0700

    kedua

---
+++ tampil.php
@@ -0,0 +1,10 @@
+<html>
+
+<head>
+    <title>Latihan pertama</title>
+</head>
+<body>
+    <p> Latihan Pertama Saya Belajar Framework  CodeIgneter</p>
+    <p>Framework CodeIgneter sangat Mudah</p>
+</body>
+</html>
